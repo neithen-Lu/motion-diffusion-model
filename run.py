39802597890510d31aa45260941a9643734a6645
Qin Dafei <qindafei@connect.hku.hk>
2023-09-04 14:02:11 +0900

    update

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import subprocess
 from subprocess import STDOUT
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 # subprocess.run(['python','-m','train.train_mdm','--save_dir','save/uncond-dp','--dataset','humanact12','--cond_mask_prob','0','--lambda_rcxyz','1','--lambda_vel','1','--lambda_fc','1 ','--unconstrained','--dependent'
 #                ], stderr=STDOUT)
 # subprocess.run(['python','-m','train.train_mdm','--save_dir','save/a2m-humanact12-base','--dataset','humanact12','--cond_mask_prob','0','--lambda_rcxyz','1','--lambda_vel','1','--lambda_fc','1 ','--eval_during_training'
 #                ], stderr=STDOUT)
-subprocess.run(['python','-m','train.train_mdm','--save_dir','save/humanml-ar-resume','--dataset','humanml','--eval_during_training','--dependent','--window_size','49','--ar_sample','--overwrite','--resume_checkpoint','/raid/HKU_TK_GROUP/qindafei/neithen/motion-diffusion-model/save/humanml-ar/model000300000.pt'
+# subprocess.run(['python','-m','train.train_mdm','--save_dir','save/humanml-ar','--dataset','humanml','--eval_during_training','--dependent','--window_size','49','--ar_sample','--overwrite'
+#                ], stderr=STDOUT)
+# subprocess.run(['python','-m','train.train_mdm','--save_dir','save/humanml-dp-losssig-2','--dataset','humanml','--eval_during_training','--dependent','--window_size','49','--loss_sig','--overwrite'
+#                ], stderr=STDOUT)
+subprocess.run(['python','-m','train.train_mdm','--save_dir','save/kit-base','--dataset','kit','--eval_during_training'
                ], stderr=STDOUT)
 # subprocess.run(['python','-m','train.train_mdm','--save_dir','save/debug','--dataset','humanml','--eval_during_training','--dependent','--window_size','28','--overwrite'
 #                ], stderr=STDOUT)
